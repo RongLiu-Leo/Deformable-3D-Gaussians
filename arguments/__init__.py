94434b60c03b8810c42a2be953927033c08c7b23
Rong Liu <102014841+RongLiu-Leo@users.noreply.github.com>
2026-01-14 17:27:23 -0800

    Update __init__.py

--- arguments/__init__.py
+++ arguments/__init__.py
@@ -60,6 +60,8 @@ class ModelParams(ParamGroup):
         self.load2gpu_on_the_fly = False
         self.is_blender = False
         self.is_6dof = False
+        self.data_loader = False
+        self.batch_size = 1
         super().__init__(parser, "Loading Parameters", sentinel)
 
     def extract(self, args):
